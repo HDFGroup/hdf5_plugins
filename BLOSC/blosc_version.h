ac4c5e05b0b78cae5a16a69ce9bfaf2909ffa016
Allen Byrne <byrn@hdfgroup.org>
2016-10-26 09:48:07 -0500

    Add interface define

--- BLOSC/blosc_version.h
+++ BLOSC/blosc_version.h
@@ -13,6 +13,7 @@
 /* Version numbers */
 #define BLOSC_PLUGIN_VERSION_MAJOR    1    /* for major interface/format changes  */
 #define BLOSC_PLUGIN_VERSION_MINOR    0   /* for minor interface/format changes  */
+#define BLOSC_PLUGIN_VERSION_INTERFACE  0    /*  */
 #define BLOSC_PLUGIN_VERSION_RELEASE  0    /* for tweaks, bug-fixes, or development */
 
 #define BLOSC_PLUGIN_VERSION_STRING   "1.0.0-dev"  /* string version.  Sync with above! */
