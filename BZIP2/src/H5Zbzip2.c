256c44088cf73ec20dfe19ec010ccbed4881c8b6
Allen Byrne <byrn@hdfgroup.org>
2017-02-28 16:04:07 -0600

    Add option for DECODE only

--- BZIP2/src/H5Zbzip2.c
+++ BZIP2/src/H5Zbzip2.c
@@ -69,7 +69,11 @@ static size_t H5Z_filter_bzip2(unsigned int flags, size_t cd_nelmts,
 const H5Z_class2_t H5Z_BZIP2[1] = {{
     H5Z_CLASS_T_VERS,       /* H5Z_class_t version */
     (H5Z_filter_t)H5Z_FILTER_BZIP2,         /* Filter id number             */
-    1,              /* encoder_present flag (set to true) */
+#ifdef FILTER_DECODE_ONLY
+    0,                   /* encoder_present flag (false is not available) */
+#else
+    1,                   /* encoder_present flag (set to true) */
+#endif
     1,              /* decoder_present flag (set to true) */
     "HDF5 bzip2 filter; see http://www.hdfgroup.org/services/contributions.html",
                                 /* Filter name for debugging    */
