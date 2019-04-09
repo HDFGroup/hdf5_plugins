0dccfd1b5cb7bc548c18e30023241f03ea37846b
Allen Byrne <byrn@hdfgroup.org>
2019-04-09 15:20:58 -0500

    HDFFV-10739 - add bitshuffle

---
+++ BSHUF/config/cmake/cacheinit.cmake
@@ -0,0 +1,31 @@
+# This is the CMakeCache file.
+
+########################
+# EXTERNAL cache entries
+########################
+
+set (BUILD_TESTING ON CACHE BOOL "Build h5bshuf Unit Testing" FORCE)
+
+set (BUILD_EXAMPLES ON CACHE BOOL "Build h5bshuf Examples" FORCE)
+
+set (HDF5_PACKAGE_NAME "hdf5" CACHE STRING "Name of HDF5 package" FORCE)
+
+set (BUILD_LZ4_LIBRARY_SOURCE ON CACHE BOOL "build the lz4 library within the plugin" FORCE)
+
+set (LZ4_GIT_URL "https://git@bitbucket.hdfgroup.org/scm/test/lz4.git" CACHE STRING "Use LZ4 from HDF repository" FORCE)
+set (LZ4_GIT_BRANCH "master" CACHE STRING "" FORCE)
+
+set (LZ4_TGZ_NAME "lz4.tar.gz" CACHE STRING "Use LZ4 from compressed file" FORCE)
+
+set (LZ4_PACKAGE_NAME "lz4" CACHE STRING "Name of LZ4 package" FORCE)
+
+set (BSHUF_GIT_URL "https://git@bitbucket.hdfgroup.org/scm/test/bshuf.git" CACHE STRING "Use BSHUF from HDF repository" FORCE)
+set (BSHUF_GIT_BRANCH "master" CACHE STRING "" FORCE)
+
+set (BSHUF_TGZ_NAME "bshuf.tar.gz" CACHE STRING "Use BSHUF from compressed file" FORCE)
+
+set (BSHUF_PACKAGE_NAME "bshuf" CACHE STRING "Name of BSHUF package" FORCE)
+set (H5BSHUF_CPACK_ENABLE ON CACHE BOOL "Enable the CPACK include and components" FORCE)
+
+set (H5PL_ALLOW_EXTERNAL_SUPPORT "NO" CACHE STRING "Allow External Library Building (NO GIT TGZ)" FORCE)
+set_property (CACHE H5PL_ALLOW_EXTERNAL_SUPPORT PROPERTY STRINGS NO GIT TGZ)
