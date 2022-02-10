75b493ee514720c653bf390e9b9aa80abf24616d
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2022-02-10 09:07:51 -0600

    Merge Update jpeg lib, namespaces and cmake (#14)
    * Merge Update jpeg lib
    
    * Update CMakeLists.txt
    
    remove duplicate code
    
    * Fix namespace

--- LZ4/config/cmake/cacheinit.cmake
+++ LZ4/config/cmake/cacheinit.cmake
@@ -10,6 +10,8 @@ set (BUILD_EXAMPLES ON CACHE BOOL "Build h5lz4 Examples" FORCE)
 
 set (HDF5_PACKAGE_NAME "hdf5" CACHE STRING "Name of HDF5 package" FORCE)
 
+set (HDF5_NAMESPACE "hdf5::" CACHE STRING "Name space of HDF5 library" FORCE)
+
 set (BUILD_LZ4_LIBRARY_SOURCE ON CACHE BOOL "build the lz4 library within the plugin" FORCE)
 
 set (LZ4_GIT_URL "https://git@bitbucket.hdfgroup.org/scm/test/lz4.git" CACHE STRING "Use LZ4 from HDF repository" FORCE)
