c4824bb589491eea4be87eb548d623542f6f3aef
Allen Byrne <byrn@hdfgroup.org>
2022-10-26 16:54:05 -0500

    Remove pre-3.14 references

--- BLOSC/config/cmake/HDFMacros.cmake
+++ BLOSC/config/cmake/HDFMacros.cmake
@@ -487,20 +487,6 @@ macro (HDF_DIR_PATHS package_prefix)
     endif ()
   endif ()
 
-#-----------------------------------------------------------------------------
-# Setup pre-3.14 FetchContent
-#-----------------------------------------------------------------------------
-  if(${CMAKE_VERSION} VERSION_LESS 3.14)
-    macro(FetchContent_MakeAvailable NAME)
-        FetchContent_GetProperties(${NAME})
-        if(NOT ${NAME}_POPULATED)
-            FetchContent_Populate(${NAME})
-            add_subdirectory(${${NAME}_SOURCE_DIR} ${${NAME}_BINARY_DIR})
-        endif()
-    endmacro()
-  endif()
-endmacro ()
-
 macro (ADD_H5_FLAGS h5_flag_var infile)
   file (STRINGS ${infile} TEST_FLAG_STREAM)
   #message (TRACE "TEST_FLAG_STREAM=${TEST_FLAG_STREAM}")
