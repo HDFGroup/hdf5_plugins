88cd28d52365bb253cf94fae3665001d58a2c0b1
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2023-02-14 12:15:57 -0600

    Changes related to 1.14 release (#50)
    * Update install code for standalone builds
    
    * Update version to match develop
    
    * Skip SZF on windows VS compiler only
    
    * Cleanup config checks
    
    * Update configure checks messages
    
    * Update docs for HDF5_ROOT change
    
    * Update compression lib
    
    * Update CMake command format
    
    * Exclude SZ filter with AppleClang
    
    * Correct compiler var name for match
    
    * remove old file
    
    * Fix typo in AppleClang check
    
    * Update for 1.13.2 release
    
    * Update to latest SZ filter
    
    * Update binary example files for latest SZ filter
    
    * Initial implementation of ZSTD filter
    
    * ZFP updates
    
    * Add support for GNUInstallDirs
    
    * Correct var name for use by HDF5
    
    * Add license file
    
    * Correct test script copyright text
    
    * remove unused dir and file
    
    * Add updates to SZ filter -  1.12.5
    
    * Add updates to SZ filter -  1.12.5
    
    * Use latest released version
    
    * Remove pre-3.14 references
    
    * Fix macro end
    
    * Skip zstd on windows unless using clang.
    
    * Exclude zstd on mac using Intel
    
    * Enable building ZSTD filter on windows.
    
    * Update dev version to 1.15 base
    
    * Restrict mingw and update version for next develop
    
    * Set default OFF for options
    
    * Fix version
    
    * Lowercase toolchain files
    
    * Correct Intel compiler names

--- JPEG/config/cmake/binex/config/toolchain/intel.cmake
+++ JPEG/config/cmake/binex/config/toolchain/intel.cmake
@@ -1,22 +1,18 @@
-#
-# Copyright by The HDF Group.
-# All rights reserved.
-#
-# This file is part of HDF5.  The full HDF5 copyright notice, including
-# terms governing use, modification, and redistribution, is contained in
-# the COPYING file, which can be found at the root of the source code
-# distribution tree, or in https://www.hdfgroup.org/licenses.
-# If you do not have access to either file, you may request a copy from
-# help@hdfgroup.org.
-#
 # Uncomment the following to use cross-compiling
-#set (CMAKE_SYSTEM_NAME Linux)
+#set(CMAKE_SYSTEM_NAME Linux)
 
 set (CMAKE_COMPILER_VENDOR "intel")
 
-set (CMAKE_C_COMPILER icc)
-set (CMAKE_CXX_COMPILER icpc)
-set (CMAKE_Fortran_COMPILER ifort)
+if (USE_LLVM)
+  set(CMAKE_C_COMPILER icx)
+  set(CMAKE_CXX_COMPILER icpx)
+  set(CMAKE_Fortran_COMPILER ifx)
+  set(INTEL_CLANG ON)
+else ()
+  set(CMAKE_C_COMPILER icc)
+  set(CMAKE_CXX_COMPILER icpc)
+  set(CMAKE_Fortran_COMPILER ifort)
+endif ()
 
 # the following is used if cross-compiling
-set (CMAKE_CROSSCOMPILING_EMULATOR "")
+set(CMAKE_CROSSCOMPILING_EMULATOR "")
