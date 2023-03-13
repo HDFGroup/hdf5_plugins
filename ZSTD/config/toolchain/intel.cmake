a0e1062cd50999e1ef2342cc3ec93353a686b34b
Allen Byrne <byrn@hdfgroup.org>
2023-03-13 15:37:18 -0500

    Fix conflicts

--- ZSTD/config/toolchain/intel.cmake
+++ ZSTD/config/toolchain/intel.cmake
@@ -3,22 +3,9 @@
 
 set(CMAKE_COMPILER_VENDOR "intel")
 
-<<<<<<< HEAD
 set(CMAKE_C_COMPILER icx)
 set(CMAKE_CXX_COMPILER icpx)
 set(CMAKE_Fortran_COMPILER ifx)
-=======
-if (USE_LLVM)
-  set(CMAKE_C_COMPILER icx)
-  set(CMAKE_CXX_COMPILER icpx)
-  set(CMAKE_Fortran_COMPILER ifx)
-  set(INTEL_CLANG ON)
-else ()
-  set(CMAKE_C_COMPILER icc)
-  set(CMAKE_CXX_COMPILER icpc)
-  set(CMAKE_Fortran_COMPILER ifort)
-endif ()
->>>>>>> branch 'master' of https://github.com/HDFGroup/hdf5_plugins.git
 
 # the following is used if cross-compiling
 set(CMAKE_CROSSCOMPILING_EMULATOR "")
