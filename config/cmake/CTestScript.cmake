eb213c3a2824c7d2dc9a48f2824c9456a187e7a9
Allen Byrne <50328838+byrnHDF@users.noreply.github.com>
2024-09-30 15:40:20 -0500

    add cdash reports tp daily-build workflow (#147)

--- config/cmake/CTestScript.cmake
+++ config/cmake/CTestScript.cmake
@@ -9,10 +9,9 @@
 # If you do not have access to either file, you may request a copy from
 # help@hdfgroup.org.
 #
-cmake_minimum_required (VERSION 3.12)
+cmake_minimum_required (VERSION 3.18)
 ########################################################
-# This dashboard is maintained by The HDF Group
-# For any comments please contact cdashhelp@hdfgroup.org
+# For any comments please contact help@hdfgroup.org
 #
 ########################################################
 # -----------------------------------------------------------
@@ -23,23 +22,22 @@ if (NOT SITE_OS_NAME)
   ## -- set hostname
   ## --------------------------
   find_program (HOSTNAME_CMD NAMES hostname)
-  exec_program (${HOSTNAME_CMD} ARGS OUTPUT_VARIABLE HOSTNAME)
+  execute_process (COMMAND ${HOSTNAME_CMD} OUTPUT_VARIABLE HOSTNAME OUTPUT_STRIP_TRAILING_WHITESPACE)
   set (CTEST_SITE  "${HOSTNAME}${CTEST_SITE_EXT}")
   find_program (UNAME NAMES uname)
   macro (getuname name flag)
-    exec_program ("${UNAME}" ARGS "${flag}" OUTPUT_VARIABLE "${name}")
+    execute_process (COMMAND "${UNAME}" "${flag}" OUTPUT_VARIABLE "${name}" OUTPUT_STRIP_TRAILING_WHITESPACE)
   endmacro ()
 
   getuname (osname -s)
+  string(STRIP ${osname} osname)
   getuname (osrel  -r)
+  string(STRIP ${osrel} osrel)
   getuname (cpu    -m)
+  string(STRIP ${cpu} cpu)
   message (STATUS "Dashboard script uname output: ${osname}-${osrel}-${cpu}\n")
 
   set (CTEST_BUILD_NAME  "${osname}-${osrel}-${cpu}")
-  if (SITE_BUILDNAME_SUFFIX)
-    set (CTEST_BUILD_NAME  "${SITE_BUILDNAME_SUFFIX}-${CTEST_BUILD_NAME}")
-  endif ()
-  set (BUILD_OPTIONS "${ADD_BUILD_OPTIONS}")
 else ()
   ## machine name provided
   ## --------------------------
@@ -48,14 +46,25 @@ else ()
   else ()
     set (CTEST_BUILD_NAME "${SITE_OS_NAME}-${SITE_OS_VERSION}-${SITE_COMPILER_NAME}")
   endif ()
-  if (SITE_BUILDNAME_SUFFIX)
-    set (CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-${SITE_BUILDNAME_SUFFIX}")
-  endif ()
-  set (BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DSITE:STRING=${CTEST_SITE} -DBUILDNAME:STRING=${CTEST_BUILD_NAME}")
+endif ()
+if (SITE_BUILDNAME_SUFFIX)
+  set (CTEST_BUILD_NAME  "${SITE_BUILDNAME_SUFFIX}-${CTEST_BUILD_NAME}")
+endif ()
+set (BUILD_OPTIONS "${ADD_BUILD_OPTIONS} -DSITE:STRING=${CTEST_SITE} -DBUILDNAME:STRING=${CTEST_BUILD_NAME}")
+
+# Launchers work only with Makefile and Ninja generators.
+if (NOT "${CTEST_CMAKE_GENERATOR}" MATCHES "Make|Ninja" OR LOCAL_SKIP_TEST)
+  set (CTEST_USE_LAUNCHERS 0)
+  set (ENV{CTEST_USE_LAUNCHERS_DEFAULT} 0)
+  set (BUILD_OPTIONS "${BUILD_OPTIONS} -DCTEST_USE_LAUNCHERS:BOOL=OFF")
+else ()
+  set (CTEST_USE_LAUNCHERS 1)
+  set (ENV{CTEST_USE_LAUNCHERS_DEFAULT} 1)
+  set (BUILD_OPTIONS "${BUILD_OPTIONS} -DCTEST_USE_LAUNCHERS:BOOL=ON")
 endif ()
 
 #-----------------------------------------------------------------------------
-# MAC machines need special option
+# MacOS machines need special options
 #-----------------------------------------------------------------------------
 if (APPLE)
   # Compiler choice
@@ -72,10 +81,10 @@ set (CTEST_CMAKE_COMMAND "\"${CMAKE_COMMAND}\"")
 ## Clear the build directory
 ## --------------------------
 set (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
-if (EXISTS "${CTEST_BINARY_DIRECTORY}" AND IS_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
-  ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
-else ()
+if (NOT EXISTS "${CTEST_BINARY_DIRECTORY}")
   file (MAKE_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
+else ()
+  ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
 endif ()
 
 # Use multiple CPU cores to build
@@ -117,22 +126,24 @@ endforeach ()
 #-----------------------------------------------------------------------------
 # Initialize the CTEST commands
 #------------------------------
-set (CTEST_CONFIGURE_TOOLSET  "")
-if(CMAKE_GENERATOR_TOOLSET)
-  set (CTEST_CONFIGURE_TOOLSET  "-T${CMAKE_GENERATOR_TOOLSET}")
+if (CMAKE_GENERATOR_TOOLSET)
+  set (CTEST_CONFIGURE_TOOLSET  "\"-T${CMAKE_GENERATOR_TOOLSET}\"")
+else ()
+  set (CTEST_CONFIGURE_TOOLSET)
 endif()
-set (CTEST_CONFIGURE_ARCHITECTURE  "")
-if(CMAKE_GENERATOR_ARCHITECTURE)
-  set (CTEST_CONFIGURE_ARCHITECTURE  "-A${CMAKE_GENERATOR_ARCHITECTURE}")
+if (CMAKE_GENERATOR_ARCHITECTURE)
+  set (CTEST_CONFIGURE_ARCHITECTURE  "\"-A${CMAKE_GENERATOR_ARCHITECTURE}\"")
+else ()
+  set (CTEST_CONFIGURE_ARCHITECTURE)
 endif()
 set (CTEST_CONFIGURE_COMMAND
-    "${CTEST_CMAKE_COMMAND} -C \"${CTEST_SOURCE_DIRECTORY}/config/cmake/cacheinit.cmake\" -DCMAKE_BUILD_TYPE:STRING=${CTEST_CONFIGURATION_TYPE} ${BUILD_OPTIONS} \"-G${CTEST_CMAKE_GENERATOR}\" \"${CTEST_CONFIGURE_ARCHITECTURE}\" \"${CTEST_CONFIGURE_TOOLSET}\" \"${CTEST_SOURCE_DIRECTORY}\""
+    "${CTEST_CMAKE_COMMAND} -C \"${CTEST_SOURCE_DIRECTORY}/config/cmake/cacheinit.cmake\" -DCMAKE_BUILD_TYPE:STRING=${CTEST_CONFIGURATION_TYPE} ${BUILD_OPTIONS} \"-G${CTEST_CMAKE_GENERATOR}\" ${CTEST_CONFIGURE_ARCHITECTURE} ${CTEST_CONFIGURE_TOOLSET} \"${CTEST_SOURCE_DIRECTORY}\""
 )
 #-----------------------------------------------------------------------------
 
 #-----------------------------------------------------------------------------
 ## -- set output to english
-set ($ENV{LC_MESSAGES}  "en_EN")
+set (ENV{LC_MESSAGES} "en_EN")
 
 # Print summary information.
 foreach (v
@@ -163,7 +174,7 @@ endif ()
   ## NORMAL process
   ## -- LOCAL_SUBMIT reports to CDash server
   ## --------------------------
-  ctest_start (${MODEL} TRACK ${MODEL})
+  ctest_start (${MODEL} GROUP ${MODEL})
   configure_file (${CTEST_SOURCE_DIRECTORY}/config/cmake/CTestCustom.cmake ${CTEST_BINARY_DIRECTORY}/CTestCustom.cmake)
   ctest_read_custom_files ("${CTEST_BINARY_DIRECTORY}")
   ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}" RETURN_VALUE res)
@@ -188,5 +199,8 @@ endif ()
   endif ()
   if (${res} LESS 0 OR ${res} GREATER 0)
     file (APPEND ${CTEST_SCRIPT_DIRECTORY}/FailedCTest.txt "Failed Tests: ${res}\n")
+  endif ()
+  if (LOCAL_SUBMIT)
+    ctest_submit (PARTS Done)
   endif ()
 #-----------------------------------------------------------------------------
