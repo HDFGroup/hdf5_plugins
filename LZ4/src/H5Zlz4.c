4eee69a3ec4a087dab632110bff8f2d92a99f05f
Aleksandar Jelenak <ajelenak@hdfgroup.org>
2025-03-24 18:52:21 -0400

    Fix LZ4 block size info

--- LZ4/src/H5Zlz4.c
+++ LZ4/src/H5Zlz4.c
@@ -74,7 +74,7 @@ static size_t H5Z_filter_lz4(unsigned int flags, size_t cd_nelmts,
 #define be64toht(x) ntohll(x)
 
 
-#define DEFAULT_BLOCK_SIZE 1<<30; /* 1GB. LZ4 needs blocks < 1.9GB. */
+#define DEFAULT_BLOCK_SIZE 1<<30; /* 1GiB. LZ4 needs blocks < 1.97GiB. */
 
 const H5Z_class2_t H5Z_LZ4[1] = {{
         H5Z_CLASS_T_VERS,       /* H5Z_class_t version */
